a6082000b93a62a622da5ce4414198cd984eff11
David Anderson <jonie4@gmail.com>
2018-01-04 23:27:07 -0800

    Implemented pointer defs for menu

--- menu.c
+++ menu.c
@@ -1,6 +1,15 @@
 #include "menu.h"
 
+void menuSetup() {
+  pointer[0]=item0;
+  pointer[1]=item1;
+  pointer[2]=exitItem;
+}
+
 void menu(){
+//  String* 
+  // if left, display array-1
+  // if right, display array+1
   return;
 }
 
--- menu.h
+++ menu.h
@@ -1,7 +1,14 @@
 #ifndef _menu_h
 #define _menu_h
 
+void menuSetup();
 void menu();
+
+#define ITEMS 3
+char item0[16] = "Set Temperature";
+char item1[15] = "Temperature Range";
+char exitItem[5] = "Exit";
+char *pointer[ITEMS];
 
 #endif
 
